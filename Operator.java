35672bef0d763b287e7a9b1e074683066c39250a
Md Azhar <mdazharstar961@gmail.com>
2024-10-17 15:52:19 +0530

    operator

---
+++ Operator.java
@@ -0,0 +1,11 @@
+package operators;
+
+public class Operator {
+	public static void main(String[] args) {
+		System.out.println(10+10);
+		System.out.println(10-10);
+		System.out.println(10*10);
+		System.out.println(10/10);
+		System.out.println(10%10);
+	}
+}
---
+++ Unary.java
@@ -0,0 +1,12 @@
+package operators;
+
+public class Unary {
+	public static void main(String[] args) {
+		int a=10;
+		System.out.println(a++); //10
+		System.out.println(++a); //12
+		System.out.println(--a); //11
+		System.out.println(a--); //11
+		System.out.println(a);   //10
+		}
+}
